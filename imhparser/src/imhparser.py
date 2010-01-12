f0654c963e5505a041bc50378b0052cbb1d83d25
Random Developer <randomdeveloper@yandex.ru>
2010-01-12 23:43:34 +0200

    Trying to parse JSON

--- imhparser/src/imhparser.py
+++ imhparser/src/imhparser.py
@@ -1,8 +1,10 @@
-from lxml.html import fromstring, tostring
+from lxml.html import fromstring
+from lxml.html import tostring
+import simplejson as json
 
 def trace(obj):
-  #soup version print unicode(obj).encode("cp1251")
-  print tostring(obj)
+    #soup version print unicode(obj).encode("cp1251")
+    print tostring(obj, encoding="cp1251")
 
 class Author:
     pass
@@ -13,25 +15,44 @@ class Item:
 
 html = open("../../books.html").read()
 
-
 doc = fromstring(html)
 
-for item in  doc.cssselect("td.'vat pl10'"):
-    trace(item)
-
+for row in  doc.cssselect("td.'vat pl10'"):
+    trace(row)
+    
     #parse author
-    authorElement = item.cssselect("span a")[0]
+    authorElement = row.cssselect("div span a")[0]
     trace(authorElement)
 
     author = Author()
-    author.Link =authorElement.attrib["href"]
-    author.Name =authorElement.text_content()
+    author.Link = authorElement.attrib["href"]
+    author.Name = authorElement.text_content()
     print author.Name
 
     #parse item
-    itemElement = item.cssselect("div a")[0]
+    itemElement = row.cssselect("div div a")[0]
     trace(itemElement)
-    
+    item = Item()
+    item.Link = itemElement.attrib["href"]
+    item.Name = itemElement.text_content()
+    print item.Name
+
+    #parse script data
+    scriptElement = row.cssselect("script")[3]
+    trace(scriptElement)
+    scriptData = scriptElement.text_content()
+    print scriptData
+
+    #get JSON data between braces {} - dirty way
+    openingBrace = scriptData.find("{")
+    closingBrace = scriptData.find("}")
+    jsonString = scriptData[openingBrace:closingBrace+1]
+    print jsonString
+
+    #test...
+    ratingData = json.loads('{user_rate:27}')    
+    print ratingData["user_rate"]
+
     break
 
 
