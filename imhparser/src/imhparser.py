c47d56ab888b159e09668b6515274a581b5d2975
Random Developer <randomdeveloper@yandex.ru>
2010-02-03 22:02:16 +0200

    Modularized code a little

--- imhparser/src/imhparser.py
+++ imhparser/src/imhparser.py
@@ -1,49 +1,16 @@
 from lxml.html import fromstring
 from lxml.html import tostring
-import yaml
-import re
+
+from ratings import *
 
 def trace(obj):
-    #soup version print unicode(obj).encode("cp1251")
     s = tostring(obj, encoding="cp1251")
     print s[:50]
-
-class Author:
-    pass
-
-class Item:
-    pass
- 
 
 html = open("../../books.html").read()
 
 doc = fromstring(html)
 
-#regex to extract JS data associated with the item
-dataJsPattern = re.compile(r"""/\* <!\[CDATA\[ \*/var params = (\{.*\}).*""")
-
-for row in  doc.cssselect("td.'vat pl10'"):    
-    #parse author
-    authorElement = row.cssselect("div span a")[0]
-
-    author = Author()
-    author.Link = authorElement.attrib["href"]
-    author.Name = authorElement.text_content()
-    print author.Name
-
-    #parse item
-    itemElement = row.cssselect("div div a")[0]
-    item = Item()
-    item.Link = itemElement.attrib["href"]
-    item.Name = itemElement.text_content()
-    print item.Name
-
-    #parse script data
-    for scriptElement in row.cssselect("script"):        
-        scriptData = scriptElement.text_content()
-        match = dataJsPattern.match(scriptData)
-        if match:
-            # not really yaml, but the fast way to get started...
-            yamlData = match.group(1).replace(":", ": ") # YAML is strict...
-            data = yaml.load(yamlData)
-            print data["user_rate"]
+for rating in ratings_from_page(doc):
+    print rating
+    break
