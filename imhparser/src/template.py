0fbf49d489b9a1da2c5624dd1c11b2ecdbb9be88
Random Developer <randomdeveloper@yandex.ru>
2010-04-26 20:23:19 +0300

    Some template parsing

--- imhparser/src/template.py
+++ imhparser/src/template.py
@@ -1,6 +1,8 @@
 
 # -*- coding: cp1251 -*-
 
+from lxml.html import fromstring
+
 template = ur"""
 h1
   title
@@ -8,21 +10,54 @@ div#HeadNavigation2 div span.nobr
   original_title
 div.element_header div.mb10 div
   short_info
-  year $".*?(\d+) год.*"
-  duration $".*?(\d+) мин.*"
+  year $.*?(\d+) год.*
+  duration $.*?(\d+) мин.*
 span#MainInfoSmallDescription
   description
 a#rates_amount_1
   rates_count
 """
 
-def dict_from_page(page, template):
-    # parser states
+class Parser:
+    def __init__(self):
+        self.elements = []
+    def extract(self, page):
+        dict = {}
+        for element in self.elements:
+            elist = page.cssselect(element.selector)
+            if elist != []:
+                e = elist[0]
+                for key, extractor in element.extractors:
+                    s = extractor.extract(e)
+                    if s:
+                        dict[key] = s
+            pass
+        return dict
+
+class Element:
+    def __init__(self, selector):
+        self.extractors = []
+        self.selector = selector
+
+class TextExtractor:
+    def extract(self, element):
+        return element.text_content()
+            
+class RegexExtractor:
+    def __init__(self, regex):
+        self.regex = regex
+    def extract(self, element):
+        return "???"
+
+def make_parser(template):
+    parser = Parser()
+
+    # parse states
     top, element = range(2)
     state = top
 
-
     last_indent = 0
+    current = None
     for line in template.splitlines():
         s = line.lstrip()
         if s == "":
@@ -32,23 +67,29 @@ def dict_from_page(page, template):
         indent = len(line) - len(s)
         if indent < last_indent:
             state = top  #must be element again
+            parser.elements.append(current)
+            current = None
         last_indent = indent
 
         if state == top:
-            selector = s
             state = element
-            print "->css selector: ", selector
+            current = Element(s)
         elif state == element:
-            t = s.partition(" ")
-            key = t[0]
-            print "key ", key
-            if t[1] == "":                
-                print "text content"
+            key, separator, tail = s.partition(" ")
+            if separator == "":
+                current.extractors.append([key, TextExtractor()])
             else:
-                tail = t[2].lstrip()
+                tail = tail.lstrip()
                 if tail[0] == "$":
-                    print "regexp ", tail
-                else:
-                    print "unknown"
+                    current.extractors.append([key, RegexExtractor(tail)])
+      
+    if ( element != None ):
+        parser.elements.append(current)
 
-dict_from_page(None, template)
+    return parser
+
+f = open("..\\..\\item.html")
+html = f.read()
+page = fromstring(html)
+parser = make_parser(template)
+print parser.extract(page)
